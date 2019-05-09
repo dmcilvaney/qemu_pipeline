05baf55e35a0707df190c90c6e7dd97635aee1c9
Daniel McIlvaney <damcilva@microsoft.com>
2019-05-09 16:34:57 -0700

    built ta

--- ftpm_test/host/main.c
+++ ftpm_test/host/main.c
@@ -43,8 +43,24 @@ typedef uint32_t TPM_CC;
 typedef uint16_t TPM_ST;
 #define TPM_ST_NO_SESSIONS					(TPM_ST)(0x8001)
 typedef uint8_t TPMI_YES_NO;
+typedef uint16_t TPM_ST;
+
+typedef uint32_t TPM_RC;
+#define TPM_RC_SUCCESS           (TPM_RC)(0x000)
 
 #pragma pack(1)
+typedef struct {
+  TPM_ST tag;
+  uint32_t paramSize;
+  TPM_CC commandCode;
+} TPM2_COMMAND_HEADER;
+
+typedef struct {
+  TPM_ST tag;
+  uint32_t paramSize;
+  TPM_RC responseCode;
+} TPM2_RESPONSE_HEADER;
+
 typedef struct {
   TPM2_COMMAND_HEADER  Header;
   TPMI_YES_NO          FullTest;
@@ -55,6 +71,27 @@ typedef struct {
 } TPM2_SELF_TEST_RESPONSE;
 #pragma pack()
 
+uint16_t
+SwapBytes16 (
+  uint16_t                    Value
+  )
+{
+  return (uint16_t) ((Value<< 8) | (Value>> 8));
+}
+
+uint32_t
+SwapBytes32 (
+  uint32_t                    Value
+  )
+{
+  uint32_t  LowerBytes;
+  uint32_t  HigherBytes;
+
+  LowerBytes  = (uint32_t) SwapBytes16 ((uint16_t) Value);
+  HigherBytes = (uint32_t) SwapBytes16 ((uint16_t) (Value >> 16));
+  return (LowerBytes << 16 | HigherBytes);
+}
+
 int main(void)
 {
 	TEEC_Result res;
@@ -63,10 +100,9 @@ int main(void)
 	TEEC_UUID uuid = TA_FTPM_UUID;
 	uint32_t err_origin;
 
-	UINT8 *SharedInput;
-	UINT8 *SharedOutput;
+	uint8_t *SharedInput;
+	uint8_t *SharedOutput;
 	TEEC_Operation TeecOperation = {0};
-	TEEC_Result TeecResult;
 	TEEC_SharedMemory TeecSharedMem = {0};
 
 	/* Initialize a context connecting us to the TEE */
@@ -80,17 +116,13 @@ int main(void)
 		errx(1, "TEEC_Opensession failed with code 0x%x origin 0x%x",
 			res, err_origin);
 
-	uint32_t                          Status;
 	TPM2_SELF_TEST_COMMAND            Cmd;
 	TPM2_SELF_TEST_RESPONSE           Res;
 
 	Cmd.Header.tag         = SwapBytes16(TPM_ST_NO_SESSIONS);
 	Cmd.Header.paramSize   = SwapBytes32(sizeof(Cmd));
 	Cmd.Header.commandCode = SwapBytes32(TPM_CC_SelfTest);
-	Cmd.FullTest           = FullTest;
-
-	ResultBufSize = sizeof(Res);
-	Status = Tpm2SubmitCommand (sizeof(Cmd), (uint8_t *)&Cmd, &ResultBufSize, (uint8_t *)&Res);
+	Cmd.FullTest           = 1;
 
 	/*
 	 * Run ftpm selftest
@@ -101,15 +133,15 @@ int main(void)
 		errx(1, "TEEC_AllocateSharedMemory failed with code 0x%x origin 0x%x",
 			res, err_origin);
 
-	SharedInput = (UINT8 *)TeecSharedMem.buffer;
+	SharedInput = (uint8_t *)TeecSharedMem.buffer;
 	SharedOutput = SharedInput + sizeof(Cmd);
 
-	CopyMem(SharedInput, InputParameterBlock, InputParameterBlockSize);
+	memcpy(SharedInput, (uint8_t*)&Cmd, sizeof(Cmd));
 
 	TeecOperation.params[0].tmpref.buffer = SharedInput;
-	TeecOperation.params[0].tmpref.size = InputParameterBlockSize;
+	TeecOperation.params[0].tmpref.size = sizeof(Cmd);
 	TeecOperation.params[1].tmpref.buffer = SharedOutput;
-	TeecOperation.params[1].tmpref.size = *OutputParameterBlockSize;
+	TeecOperation.params[1].tmpref.size = sizeof(Res);
 	TeecOperation.paramTypes = TEEC_PARAM_TYPES(TEEC_MEMREF_TEMP_INPUT,
 											  TEEC_MEMREF_TEMP_INOUT,
 											  TEEC_NONE,
@@ -120,7 +152,12 @@ int main(void)
 	if (res != TEEC_SUCCESS)
 		errx(1, "TEEC_InvokeCommand failed with code 0x%x origin 0x%x",
 			res, err_origin);
-	printf("TA incremented value to %d\n", op.params[0].value.a);
+
+	memcpy((uint8_t*)&Res, SharedOutput, sizeof(Res));
+	printf("fTPM TA selftest returned %x\n", Res.Header.responseCode);
+
+	if (Res.Header.responseCode != TPM_RC_SUCCESS)
+		errx(1, "TPM failed with code 0x%x", Res.Header.responseCode);
 
 	/*
 	 * We're done with the TA, close the session and
