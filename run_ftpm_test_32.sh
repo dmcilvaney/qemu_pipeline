c3554483636442fa0636f27563ee22e051ea4b8b
Daniel McIlvaney <damcilva@microsoft.com>
2019-05-24 17:38:59 -0700

    Update pipes

--- run_ftpm_test_32.sh
+++ run_ftpm_test_32.sh
@@ -3,13 +3,16 @@
 set -e
 
 mkdir -p logs
-echo "QEMU LOG:" > ./logs/qemu.log
-echo "REE LOG:" > ./logs/ree.log
-echo "TEE LOG:" > ./logs/tee.log
 
 rm -f /tmp/ree.in /tmp/ree.out /tmp/tee.in /tmp/tee.out
 mkfifo /tmp/ree.in /tmp/ree.out /tmp/tee.in /tmp/tee.out
 
+echo "REE LOG:" > ./logs/ree.log
+echo "TEE LOG:" > ./logs/tee.log
+nohup cat /tmp/ree.out >> ./logs/ree.log 2>&1 &
+nohup cat /tmp/tee.out >> ./logs/tee.log 2>&1 &
+
+echo "QEMU LOG:" > ./logs/qemu.log
 nohup ./qemu-system-arm \
     -nographic \
     -serial pipe:/tmp/ree -serial pipe:/tmp/tee \
@@ -19,11 +22,6 @@ nohup ./qemu-system-arm \
     -m 1057 \
     -bios bl1.bin \
     -virtfs local,id=sh0,path=$QEMU_MOUNT_DIR,security_model=passthrough,readonly,mount_tag=sh0 >> ./logs/qemu.log 2>&1 &
-
-nohup cat /tmp/ree.out >> ./logs/ree.log 2>&1 &
-CAT_PID=$!
-
-nohup cat /tmp/tee.out >> ./logs/tee.log 2>&1 &
 
 # Wait for QEMU to boot
 echo QEMU started
@@ -51,12 +49,9 @@ echo "$QEMU_CMD" > /tmp/ree.in
 echo "Command sent"
 #Seperate the results from the login and command logs
 sleep 1
-echo "Switching output files (killing $CAT_PID)"
-kill $CAT_PID
-echo "Killed"
-wait $CAT_PID 2> /dev/null || echo "Wait for kill failed?"
+
 echo "RESULTS LOG:" > ./logs/tee.log
-nohup cat /tmp/ree.in >> ./logs/results.log 2>&1 &
+nohup tail -f -n 0 /tmp/ree.in >> ./logs/results.log 2>&1 &
 
 echo "Waiting for tests to finish"
 while ! grep "$QEMU_TEST_FLAG" ./logs/results.log > /dev/null; do
