d5ef9d6f60e6fc64272721314e4a2bd4b0e0bfdf
Daniel McIlvaney <damcilva@microsoft.com>
2019-05-24 17:20:44 -0700

    Switch to pipes instead of pts

--- run_ftpm_test_32.sh
+++ run_ftpm_test_32.sh
@@ -7,9 +7,12 @@ echo "QEMU LOG:" > ./logs/qemu.log
 echo "REE LOG:" > ./logs/ree.log
 echo "TEE LOG:" > ./logs/tee.log
 
+rm -f /tmp/ree.in /tmp/ree.out /tmp/tee.in /tmp/tee.out
+mkfifo /tmp/ree.in /tmp/ree.out /tmp/tee.in /tmp/tee.out
+
 nohup ./qemu-system-arm \
     -nographic \
-    -serial pty -serial pty \
+    -serial pipe:/tmp/ree -serial pipe:/tmp/ree \
     -smp 1 \
     -machine virt -machine secure=on -cpu cortex-a15 \
     -d unimp  -semihosting-config enable,target=native \
@@ -17,21 +20,18 @@ nohup ./qemu-system-arm \
     -bios bl1.bin \
     -virtfs local,id=sh0,path=$QEMU_MOUNT_DIR,security_model=passthrough,readonly,mount_tag=sh0 >> ./logs/qemu.log 2>&1 &
 
-sleep 1
-QEMU_PTS=$(grep --max-count=2 -o "/dev/pts/[^ ]*" ./logs/qemu.log | sed -n 1p)
-TEE_PTS=$(grep --max-count=2 -o "/dev/pts/[^ ]*" ./logs/qemu.log | sed -n 2p)
-
-nohup cat $QEMU_PTS >> ./logs/ree.log 2>&1 &
+nohup cat /tmp/ree.out >> ./logs/ree.log 2>&1 &
 CAT_PID=$!
-nohup cat $TEE_PTS >> ./logs/tee.log 2>&1 &
+
+nohup cat /tmp/tee.out >> ./logs/tee.log 2>&1 &
 
 # Wait for QEMU to boot
 echo QEMU started
 sleep 20
 
 # Log into root account
-echo Attempting to connect to QEMU at $QEMU_PTS
-echo "root" > $QEMU_PTS
+echo Attempting to connect to QEMU at /tmp/ree.in
+echo "root" > /tmp/ree.in
 sleep 5
 
 QEMU_TEST_FLAG="QEMU TEST COMPLETE"
@@ -45,7 +45,7 @@ echo "Running command:"
 echo ""
 echo $QEMU_CMD
 echo ""
-echo "$QEMU_CMD" > $QEMU_PTS
+echo "$QEMU_CMD" > /tmp/ree.in
 echo "Command sent"
 #Seperate the results from the login and command logs
 sleep 1
@@ -54,7 +54,7 @@ kill $CAT_PID
 echo "Killed"
 wait $CAT_PID 2> /dev/null || echo "Wait for kill failed?"
 echo "RESULTS LOG:" > ./logs/tee.log
-nohup cat $QEMU_PTS >> ./logs/results.log 2>&1 &
+nohup cat /tmp/ree.in >> ./logs/results.log 2>&1 &
 
 echo "Waiting for tests to finish"
 while ! grep "$QEMU_TEST_FLAG" ./logs/results.log > /dev/null; do
@@ -75,8 +75,11 @@ if ! grep "fTPM TA selftest returned 0" ./logs/results.log > /dev/null; then
     echo "Results:"
     echo ""
     cat ./logs/results.log
+    
+    rm -f /tmp/ree.in /tmp/ree.out /tmp/tee.in /tmp/tee.out
     exit 1
 fi
 
+rm -f /tmp/ree.in /tmp/ree.out /tmp/tee.in /tmp/tee.out
 echo "Done!"
 cat ./logs/results.log
