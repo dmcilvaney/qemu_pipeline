1633f3a3b05ae6d20298aeaacf00bf0cb25fe7fd
Daniel McIlvaney <damcilva@microsoft.com>
2019-05-28 13:11:10 -0700

    Ready for test

--- run_ftpm_test_32.sh
+++ run_ftpm_test_32.sh
@@ -4,23 +4,23 @@ set -e
 
 mkdir -p logs
 
-rm -f /tmp/ree.in /tmp/ree.out /tmp/tee.in /tmp/tee.out
-mkfifo /tmp/ree.in /tmp/ree.out /tmp/tee.in /tmp/tee.out
+rm -f /tmp/ree_32.in /tmp/ree_32.out /tmp/tee_32.in /tmp/tee_32.out
+mkfifo /tmp/ree_32.in /tmp/ree_32.out /tmp/tee_32.in /tmp/tee_32.out
 
 echo "REE LOG:" > ./logs/ree.log
 echo "TEE LOG:" > ./logs/tee.log
-nohup cat /tmp/ree.out >> ./logs/ree.log 2>&1 &
-nohup cat /tmp/tee.out >> ./logs/tee.log 2>&1 &
+nohup cat /tmp/ree_32.out >> ./logs/ree_32.log 2>&1 &
+nohup cat /tmp/tee_32.out >> ./logs/tee_32.log 2>&1 &
 
 echo "QEMU LOG:" > ./logs/qemu.log
 
 echo "Mounting directory at $QEMU_MOUNT_DIR"
 QEMU_CMD="./qemu-system-arm \
     -nographic \
-    -serial pipe:/tmp/ree -serial pipe:/tmp/tee \
+    -serial pipe:/tmp/ree_32 -serial pipe:/tmp/tee_32 \
     -smp 1 \
     -machine virt -machine secure=on -cpu cortex-a15 \
-    -d unimp  -semihosting-config enable,target=native \
+    -d unimp -semihosting-config enable,target=native \
     -m 1057 \
     -bios bl1.bin \
     -fsdev local,id=fsdev0,path=$QEMU_MOUNT_DIR,security_model=none -device virtio-9p-device,fsdev=fsdev0,mount_tag=host"
@@ -34,14 +34,14 @@ while ! grep "buildroot login:" ./logs/ree.log > /dev/null; do
 done
 
 # Log into root account
-echo Attempting to connect to QEMU at /tmp/ree.in
-echo "root" > /tmp/ree.in
+echo Attempting to connect to QEMU at /tmp/ree_32.in
+echo "root" > /tmp/ree_32.in
 sleep 5
 
 QEMU_TEST_FLAG="QEMU TEST COMPLETE"
 QEMU_CMD=" sleep 5 && \
     mkdir /mnt/ci && \
-    mount -t 9p -o trans=virtio sh0 /mnt/ci -oversion9p2000.L && \
+    mount -t 9p -o trans=virtio host /mnt/ci && \
     cp /mnt/ci/*.ta /lib/optee_armtz && \
     /mnt/ci/ftpm_test; echo $QEMU_TEST_FLAG"
 
@@ -49,13 +49,13 @@ echo "Running command:"
 echo ""
 echo $QEMU_CMD
 echo ""
-echo "$QEMU_CMD" > /tmp/ree.in
+echo "$QEMU_CMD" > /tmp/ree_32.in
 echo "Command sent"
 #Seperate the results from the login and command logs
 sleep 1
 
-echo "RESULTS LOG:" > ./logs/tee.log
-nohup tail -f -n 0 /tmp/ree.in >> ./logs/results.log 2>&1 &
+echo "RESULTS LOG:" > ./logs/reults.log
+nohup tail -f -n 0 /tmp/ree_32.in >> ./logs/results.log 2>&1 &
 
 echo "Waiting for tests to finish"
 while ! grep "$QEMU_TEST_FLAG" ./logs/results.log > /dev/null; do
@@ -77,10 +77,10 @@ if ! grep "fTPM TA selftest returned 0" ./logs/results.log > /dev/null; then
     echo ""
     cat ./logs/results.log
     
-    rm -f /tmp/ree.in /tmp/ree.out /tmp/tee.in /tmp/tee.out
+    rm -f /tmp/ree_32.in /tmp/ree_32.out /tmp/tee_32.in /tmp/tee_32.out
     exit 1
 fi
 
-rm -f /tmp/ree.in /tmp/ree.out /tmp/tee.in /tmp/tee.out
+rm -f /tmp/ree_32.in /tmp/ree_32.out /tmp/tee_32.in /tmp/tee_32.out
 echo "Done!"
 cat ./logs/results.log
--- run_ftpm_test_64.sh
+++ run_ftpm_test_64.sh
@@ -3,42 +3,46 @@
 set -e
 
 mkdir -p logs
-nohup ./qemu-system-aarch64 \
+
+rm -f /tmp/ree_64.in /tmp/ree_64.out /tmp/tee_64.in /tmp/tee_64.out
+mkfifo /tmp/ree_64.in /tmp/ree_64.out /tmp/tee_64.in /tmp/tee_64.out
+
+echo "REE LOG:" > ./logs/ree.log
+echo "TEE LOG:" > ./logs/tee.log
+nohup cat /tmp/ree_64.out >> ./logs/ree_64.log 2>&1 &
+nohup cat /tmp/tee_64.out >> ./logs/tee_64.log 2>&1 &
+
+echo "QEMU LOG:" > ./logs/qemu.log
+
+echo "Mounting directory at $QEMU_MOUNT_DIR"
+QEMU_CMD="./qemu-system-aarch64 \
     -nographic \
-    -serial pty -serial pty \
+    -serial pipe:/tmp/ree_64 -serial pipe:/tmp/tee_64 \
     -smp 1 \
     -machine virt,secure=on -cpu cortex-a57 \
     -d unimp -semihosting-config enable,target=native \
     -m 1057 \
     -bios bl1.bin \
     -initrd rootfs.cpio.gz -kernel Image -no-acpi \
-    -append 'console=ttyAMA0,38400 keep_bootcon root=/dev/vda2' \
-    -virtfs local,id=sh0,path=$QEMU_MOUNT_DIR,security_model=passthrough,readonly,mount_tag=sh0 > ./logs/qemu.log 2>&1 &
-    
-#-netdev user,id=vmnic -device virtio-net-device,netdev=vmnic
-
-
-sleep 1
-QEMU_PTS=$(grep --max-count=2 -o "/dev/pts/[^ ]*" ./logs/qemu.log | sed -n 1p)
-TEE_PTS=$(grep --max-count=2 -o "/dev/pts/[^ ]*" ./logs/qemu.log | sed -n 2p)
-
-nohup cat $QEMU_PTS > ./logs/ree.log 2>&1 &
-CAT_PID=$!
-nohup cat $TEE_PTS > ./logs/tee.log 2>&1 &
+    -fsdev local,id=fsdev0,path=$QEMU_MOUNT_DIR,security_model=none -device virtio-9p-device,fsdev=fsdev0,mount_tag=host"
+echo "Command:  $QEMU_CMD" >> ./logs/qemu.log
+nohup $QEMU_CMD >> ./logs/qemu.log 2>&1 &
 
 # Wait for QEMU to boot
 echo QEMU started
-sleep 20
+while ! grep "buildroot login:" ./logs/ree.log > /dev/null; do
+    sleep 1
+done
 
 # Log into root account
-echo Attempting to connect to QEMU at $QEMU_PTS
-echo "root" > $QEMU_PTS
+echo Attempting to connect to QEMU at /tmp/ree_64.in
+echo "root" > /tmp/ree_64.in
 sleep 5
 
 QEMU_TEST_FLAG="QEMU TEST COMPLETE"
 QEMU_CMD=" sleep 5 && \
     mkdir /mnt/ci && \
-    mount -t 9p -o trans=virtio sh0 /mnt/ci -oversion9p2000.L && \
+    mount -t 9p -o trans=virtio host /mnt/ci && \
     cp /mnt/ci/*.ta /lib/optee_armtz && \
     /mnt/ci/ftpm_test; echo $QEMU_TEST_FLAG"
 
@@ -46,13 +50,13 @@ echo "Running command:"
 echo ""
 echo $QEMU_CMD
 echo ""
-echo "$QEMU_CMD" > $QEMU_PTS
-
+echo "$QEMU_CMD" > /tmp/ree_64.in
+echo "Command sent"
 #Seperate the results from the login and command logs
 sleep 1
-kill $CAT_PID
-wait $CAT_PID 2> /dev/null
-nohup cat $QEMU_PTS > ./logs/results.log 2>&1 &
+
+echo "RESULTS LOG:" > ./logs/tee.log
+nohup tail -f -n 0 /tmp/ree_64.in >> ./logs/results.log 2>&1 &
 
 echo "Waiting for tests to finish"
 while ! grep "$QEMU_TEST_FLAG" ./logs/results.log > /dev/null; do
@@ -73,8 +77,11 @@ if ! grep "fTPM TA selftest returned 0" ./logs/results.log > /dev/null; then
     echo "Results:"
     echo ""
     cat ./logs/results.log
+    
+    rm -f /tmp/ree_64.in /tmp/ree_64.out /tmp/tee_64.in /tmp/tee_64.out
     exit 1
 fi
 
+rm -f /tmp/ree_64.in /tmp/ree_64.out /tmp/tee_64.in /tmp/tee_64.out
 echo "Done!"
 cat ./logs/results.log
